b3ab7eda429a312071af6d361ef00e4e2660f466
. <fenik400@gmail.com>
2025-11-05 22:36:13 +0300

    .

---
+++ src/main/java/org/legion/easyKarma/EasyKarma.java
@@ -0,0 +1,17 @@
+package org.legion.easyKarma;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyKarma extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
